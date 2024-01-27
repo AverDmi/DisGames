2f8ee0d01d7e18bd5a017f9bed3ac033ea562fd4
Dmitriy Averyanov <dimthomas1989@gmail.com>
2024-01-27 18:46:32 +0300

    Added viewBinding

--- app/src/main/java/com/dimthomas/disgames/presentation/MainActivity.kt
+++ app/src/main/java/com/dimthomas/disgames/presentation/MainActivity.kt
@@ -3,10 +3,15 @@ package com.dimthomas.disgames.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dimthomas.disgames.R
+import com.dimthomas.disgames.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
